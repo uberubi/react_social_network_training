c5620056772a2c23f33813331f01c3d8d35ccc55
Andrew A. Stenipanin <and5251@yandex.ru>
2020-04-09 18:44:44 +0300

    hooks, useState, useEffect

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import s from "./ProfileInfo.module.css";
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatus from './ProfileStatus'
+import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
 
 const ProfileInfo = props => {
   if (!props.profile) {
@@ -15,7 +16,7 @@ const ProfileInfo = props => {
       </div> */}
       <div className={s.descriptionBlock}>
         <img  className={s.userAvatar} src={props.profile.photos.large} alt="profile"/>
-        <ProfileStatus status={props.status} updateStatus={props.updateStatus}/>
+        <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
         </div>
     </div>
   );
