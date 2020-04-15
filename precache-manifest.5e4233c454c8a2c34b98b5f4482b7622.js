0a0fe17e554d23231042356b0cc80697438d7211
Andrew A. Stenipanin <and5251@yandex.ru>
2020-04-15 23:35:25 +0300

    Updates

---
+++ precache-manifest.5e4233c454c8a2c34b98b5f4482b7622.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0c18a4df73d8926d15d63fb6ede0a6e",
+    "url": "/react_social_network_training/index.html"
+  },
+  {
+    "revision": "90e3d675820f08d9a259",
+    "url": "/react_social_network_training/static/css/3.9383ef2b.chunk.css"
+  },
+  {
+    "revision": "8a02541abadc3653990f",
+    "url": "/react_social_network_training/static/css/4.419a9e04.chunk.css"
+  },
+  {
+    "revision": "5cbefb80bed8f67db5ea",
+    "url": "/react_social_network_training/static/css/main.09db39e0.chunk.css"
+  },
+  {
+    "revision": "c05391d04bf07d7f70fa",
+    "url": "/react_social_network_training/static/js/2.df9f50f7.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react_social_network_training/static/js/2.df9f50f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90e3d675820f08d9a259",
+    "url": "/react_social_network_training/static/js/3.46361a1d.chunk.js"
+  },
+  {
+    "revision": "8a02541abadc3653990f",
+    "url": "/react_social_network_training/static/js/4.1f87cb1d.chunk.js"
+  },
+  {
+    "revision": "5cbefb80bed8f67db5ea",
+    "url": "/react_social_network_training/static/js/main.7c013b53.chunk.js"
+  },
+  {
+    "revision": "0578ec9374803ce24d26",
+    "url": "/react_social_network_training/static/js/runtime-main.9b67ec02.js"
+  },
+  {
+    "revision": "9c3e176beee235c140a7c9c08fd6c519",
+    "url": "/react_social_network_training/static/media/preloader.9c3e176b.svg"
+  },
+  {
+    "revision": "e95544babc19eb9a51416fe10b4f8a2e",
+    "url": "/react_social_network_training/static/media/user_img.e95544ba.jpg"
+  }
+]);
