051c5b7d49f52ec1d23c3dc645069c2cfc76113f
Andrew A. Stenipanin <and5251@yandex.ru>
2020-08-06 22:50:14 +0300

    add login redux forms ts

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,20 +1,25 @@
-import React from "react";
-import { reduxForm, Field } from "redux-form";
+import React, {FC} from "react";
+import { reduxForm, InjectedFormProps } from "redux-form";
 import { Input, createField } from "../common/FormsControls/FormControls";
 import { required } from "../../utils/validators/validators";
 import { connect } from "react-redux";
 import { login } from "../../redux/auth-reducer";
 import { Redirect } from "react-router-dom";
 import style from "./../common/FormsControls/FormsControls.module.css";
+import { AppStateType } from "../../redux/redux-store";
 
-const LoginForm = ({handleSubmit, error, captchaUrl}) => {
+type LoginFormOwnProps = {
+  captchaUrl: string | null
+}
+
+const LoginForm: FC<InjectedFormProps<LoginFormValuesType, LoginFormOwnProps> & LoginFormOwnProps> = ({handleSubmit, error, captchaUrl}) => {
   return (
     <form onSubmit={handleSubmit}>
         {createField("Email", "email", [required], Input)}
         {createField("Password", "password", [required], Input,{type: 'password'} )}
-        {createField(null, "rememberMe", [], Input,{type: 'checkbox'},'remember me')}
+        {createField(undefined, "rememberMe", [], Input,{type: 'checkbox'},'remember me')}
 
-        {captchaUrl && <img src={captchaUrl} />}
+        {captchaUrl && <img src={captchaUrl} alt="captcha"/>}
         {captchaUrl && createField("Symbols from image", "captcha", [required], Input, {})}
 
       {error && <div className={style.formSummaryError}>{error}</div>}
@@ -25,12 +30,29 @@ const LoginForm = ({handleSubmit, error, captchaUrl}) => {
   );
 };
 
-const LoginReduxForm = reduxForm({
+const LoginReduxForm = reduxForm<LoginFormValuesType, LoginFormOwnProps>({
   form: "login",
 })(LoginForm);
 
-const Login = (props) => {
-  const onSubmit = (formData) => {
+
+type MapDispatchPropsType = {
+  login: (email: string, password: string, rememberMe: boolean, captcha: string) => void
+}
+
+type MapStatePropsType = {
+  captchaUrl: string | null
+  isAuth: boolean
+}
+
+type LoginFormValuesType = {
+  email: string
+  password: string
+  rememberMe: boolean
+  captcha: string
+}
+
+const Login: FC<MapStatePropsType & MapDispatchPropsType> = (props) => {
+  const onSubmit = (formData: any) => {
     props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
     console.log(formData);
   };
@@ -46,7 +68,7 @@ const Login = (props) => {
   );
 };
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = (state: AppStateType): MapStatePropsType => ({
   captchaUrl: state.auth.captchaUrl,
   isAuth: state.auth.isAuth,
 });
